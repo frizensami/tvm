bdb891895b470cf8d0645a4ac7d7d681baec3ed3
Sriram Sami <frizensami@gmail.com>
2016-01-11 17:02:30 +0800

    Check if wave has already started, errors are returned to notify volunteer if they try to update a participant to a wave that has already started

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,6 +1,16 @@
 class Participant < ActiveRecord::Base
 	validates :name, :bib_number, :presence => true
 
+  #cannot input a wave that has already started
+  validate :wave_cannot_have_started
+
 	#PARANOID MODE: Nothing really gets deleted
 	acts_as_paranoid
+
+  def wave_cannot_have_started
+    if Wave.exists?(wave_number: wave_number)
+      errors.add(:wave_number, "is not allowed. Wave has already started.")
+    end
+  end
+
 end
