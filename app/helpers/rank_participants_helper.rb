d615f632f4a676a37ddb1cb34370212d1a58b6bf
Sriram Sami <frizensami@gmail.com>
2016-01-18 20:31:04 +0800

    Centered name field, updated search button to reflect bib number, removed bug with rankparticipants missing number when no rank participants

--- app/helpers/rank_participants_helper.rb
+++ app/helpers/rank_participants_helper.rb
@@ -1,5 +1,7 @@
 module RankParticipantsHelper
   def missing_numbers
-    return (Rank.minimum(:rank)...Rank.maximum(:rank)).to_a - Rank.pluck(:rank)
+    if Rank.count > 0
+      return (Rank.minimum(:rank)...Rank.maximum(:rank)).to_a - Rank.pluck(:rank)
+    end
   end
 end
